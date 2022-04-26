30b6d29ab33da7dd1ead27e86353baa418b98bcb
Lexyth <Lexyth@users.noreply.github.com>
2022-04-26 19:20:17 +0200

    Update main.js

--- docs/js/main.js
+++ docs/js/main.js
@@ -171,7 +171,9 @@ document.addEventListener("DOMContentLoaded", function () {
       .pixelate();
 
     alert("before watermark");
-    alert(px.ctx);
+    alert(px.toString());
+    alert(px.ctx.toString());
+    alert(px.ctx.toDataUrl().toString());
     alert(watermark.toString());
     alert("v18:49");
     //watermark([px.ctx.toDataUrl(), 'assets/B4099C75-5E9E-427F-B896-58BC1BC7AC87.png'])
